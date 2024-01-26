7449e48733faa711ea50f103de869922f5e78800
Farid <farid.talebloo@ucalgary.ca>
2024-01-26 10:24:46 -0700

    the file is created.

---
+++ 003_write_a_function.py
@@ -0,0 +1,11 @@
+# link: https://www.hackerrank.com/challenges/write-a-function/problem
+
+def is_leap(year):
+    leap = False
+    
+    # Write your logic here
+    
+    return leap
+
+year = int(input())
+print(is_leap(year))
