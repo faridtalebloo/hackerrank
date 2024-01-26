394935af1fd0123e9ff045c2baa3b75274b1b6a0
Farid <farid.talebloo@ucalgary.ca>
2024-01-26 12:39:34 -0700

    "write a function" is solved.

--- 003_write_a_function.py
+++ 003_write_a_function.py
@@ -3,7 +3,7 @@
 def is_leap(year):
     leap = False
     
-    # Write your logic here
+    leap = True if (year % 4 ==0 and year % 100 != 0) or (year % 400 == 0) != 0 else False
     
     return leap
 
