89df2a84df8c3f78416639a7d476b2566a256f00
Farid <farid.talebloo@ucalgary.ca>
2024-01-31 17:46:14 -0700

    finding the percentage is solved.

--- 008_finding-the-percentage.py
+++ 008_finding-the-percentage.py
@@ -1,2 +1,15 @@
 # link: https://www.hackerrank.com/challenges/finding-the-percentage/problem
 
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    result = 0 
+    for i in student_marks[query_name]:
+        result += i
+    result /= len(student_marks[query_name])
+    print(format(result,".2f"))
